9c566d471f71e7f3a57570f352fd3e8a1862d2bb
justinovation <jaddeleon@mymail.mapua.edu.ph>
2026-01-04 03:36:26 +0800

    feat(python): solve 6.100L problem set 0

---
+++ python-core/6.100L/mit6_100I_f22_ps0_code/ps0.py
@@ -0,0 +1,11 @@
+## 6.100A PSet 0
+## Name: Justin Andre De Leon
+## Time Spent:
+## Collaborators:
+    
+import numpy
+
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+print(f"X**y = {x**y}")
+print(f"log(x) = {int(numpy.log2(x))}")
