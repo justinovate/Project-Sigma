96c202fe12cad53454bb5b0cf3c1fbad9abec6a5
justinovation <jaddeleon@mymail.mapua.edu.ph>
2026-02-14 23:44:55 +0800

    feat(python): complete exercise (prime)

---
+++ python-core/exercises/prime.py
@@ -0,0 +1,11 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+num = int(input("Enter a number:"))
+result = is_prime(num)
+print(f"{num} is {'a prime number' if result else 'not a prime number'}")
