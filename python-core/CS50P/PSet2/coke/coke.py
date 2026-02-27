fa7c51a233d9a10548ea1496fefbbd6d2cfe20b9
justinovate <jaddeleon@mymail.mapua.edu.ph>
2026-02-27 13:26:16 +0800

    feat(python): initialize coke.py

---
+++ python-core/CS50P/PSet2/coke/coke.py
@@ -0,0 +1,11 @@
+# amount_due = 50
+# total_payment = 0
+
+# while amount_due > total_payment:
+#     print(f"Amount Due: {amount_due}")
+#     payment = int(input("Insert Coin: "))
+#     if payment in [25, 10, 5]:
+#         amount_due -= payment
+#         if payment > amount_due:
+#             print(f"Change Owed: {payment - amount_due}")
+#     total_payment += payment
