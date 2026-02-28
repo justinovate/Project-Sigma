0d002f88bb84b982b9ceae1241b0491baf053470
justinovate <jaddeleon@mymail.mapua.edu.ph>
2026-02-28 12:24:50 +0800

    feat(python): complete coke problem

--- python-core/CS50P/PSet2/coke/coke.py
+++ python-core/CS50P/PSet2/coke/coke.py
@@ -1,9 +1,11 @@
-# amount_due = 50
+amount_due = 50
 
-# while amount_due > 0:
-#     print(f"Amount Due: {amount_due}")
-#     payment = int(input("Insert Coin: "))
-#     if payment in [25, 10, 5]:
-#         amount_due -= payment
-#         change = payment - amount_due
-# print(f"Change Owed: {change}")
+while amount_due > 0:
+    print(f"Amount Due: {amount_due}")
+    payment = int(input("Insert Coin: "))
+    
+    if payment in {5, 10, 25}:
+        amount_due -= payment
+
+change = abs(amount_due)
+print(f"Change Owed: {change}")
