95cedc9fd82ff6cf4dbdf58cd7af62ae293128e7
justinovate <jaddeleon@mymail.mapua.edu.ph>
2026-03-12 15:04:10 +0800

    lec(python): introduce dics and list of dict

--- python-core/CS50P/Lec2/hogwarts.py
+++ python-core/CS50P/Lec2/hogwarts.py
@@ -1,4 +1,27 @@
-students = ["Hermione", "Harry", "Ron"]
+### List
+# students = ["Hermione", "Harry", "Ron"]
 
-for i in range(len(students)):
-    print(i + 1, students[i])
+# for i in range(len(students)):
+#     print(i + 1, students[i])
+
+
+### Single Dict
+# students = {
+#     "Hermione": "Gryffindor",
+#     "Harry": "Gryffindor",
+#     "Ron": "Gryffindor",
+#     "Draco": "Slytherin"
+# }
+
+# for student in students:
+#     print(student, students[student], sep=", ")
+
+students = [
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell Terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None}
+]
+
+for student in students:
+    print(student["name"], student["house"], student["patronus"], sep=", ")
