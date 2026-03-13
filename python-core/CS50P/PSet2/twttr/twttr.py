c4d995e616ee93789353200f963eb523cc1f2625
justinovate <jaddeleon@mymail.mapua.edu.ph>
2026-03-13 15:36:37 +0800

    fix: simplify code for twttr.py

--- python-core/CS50P/PSet2/twttr/twttr.py
+++ python-core/CS50P/PSet2/twttr/twttr.py
@@ -1,9 +1,7 @@
-prompt = input("Input: ")
-output = ""
-for character in prompt:
-    if character in ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']:
-        character = ""
-        output += character
+user_input = input("Input: ")
+print("Output: ", end="")
+for c in user_input:
+    if c.lower() in ["a", "e", "i", "o", "u"]:
+        continue
     else:
-        output += character
-print(f"Output: {output}")
+        print(c, end="")
