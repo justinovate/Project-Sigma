0e9885e2277375fee9d6aeae3f85d3f597938bd9
justinovate <jaddeleon@mymail.mapua.edu.ph>
2026-03-15 19:24:04 +0800

    fix: edit conditions in is_valid() function

--- python-core/CS50P/PSet2/plates/plates.py
+++ python-core/CS50P/PSet2/plates/plates.py
@@ -5,13 +5,34 @@ def main():
     else:
         print("Invalid")
 
+# def is_valid(s):
+#     if s[:2].isalpha():
+#         if 2 <= len(s) <= 6:
+#             if s.isalnum():
+#                 for i in range(len(s)):
+#                     if s[i].isnumeric() and int(s[i]) > 0:
+#                         if s[i:].isnumeric():
+#                             return True
+
 def is_valid(s):
-    if s[:2].isalpha():
-        if 2 <= len(s) <= 6:
-            if s.isalnum():
-                for i in range(len(s)):
-                    if s[i].isnumeric() and int(s[i]) > 0:
-                        if s[i:].isnumeric():
-                            return True
+    conditions = [
+        s.isalnum(),
+        s[:2].isalpha(),
+        2 <= len(s) <= 6,
+    ]
+
+    number_started = False
+    
+    for c in s[2:]:
+        if c.isdigit():
+            if not number_started:
+                number_started = True
+                if c == '0':
+                    return False
+        elif c.isalpha():
+            if number_started:
+                return False
+            
+    return all(conditions)
 
 main()
