52551cb557fd793fd4e9eb793df352e32cbdd7cc
justinovate <jaddeleon@mymail.mapua.edu.ph>
2026-03-18 22:19:06 +0800

    lec(python): exceptions

---
+++ python-core/CS50P/Lec3/number.py
@@ -0,0 +1,18 @@
+def main():
+    x = get_int("What's x? ")
+    print(f"x is {x}")
+
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt)) 
+            # break
+        except ValueError:
+            # print("x is not an integer")
+            pass
+        # else:
+        #     # break
+        #     return x
+
+main()
+
